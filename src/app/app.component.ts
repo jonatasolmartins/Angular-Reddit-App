1febb51a7c672e7f9de38d1a37550c60fa68e8e8
Jonatas <jonatasolmartins@outlook.com>
2019-02-07 01:31:26 -0200

    Criado novo componente SidebarComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div id="container">
+    <app-sidebar></app-sidebar>
+    <div id="content">      
+      <app-article *ngFor="let article of articles" [article]="article">
+      </app-article>            
+    </div>
+  </div>
+ `
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: object[];
+
+  constructor(){
+    this.articles = [
+    {
+      title: 'The Angular 2 screencast',
+      description: 'The easiest way to learn Angular 2'
+    },
+    {
+      title: 'Fullstack React',
+      description: 'Wanna learn React as well?'
+    }]
+  }
+
 }
+
+@Component({
+  selector: 'app-sidebar',
+  template: `
+  <div id="sidebar">
+  Sidebar will go here
+  </div>
+   `
+})
+export class SidebarComponent {}
