75eb2a12e4d0c97944af0b6bb0cbc0b91a86d8dd
Jonatas <jonatasolmartins@outlook.com>
2019-02-07 01:32:40 -0200

    Adicionado os novos componentes ao appModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent, SidebarComponent } from './app.component';
+import { ArticleComponent } from './component/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
